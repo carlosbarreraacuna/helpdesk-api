302acc298b4dcc89be212c46123db4385dbcd1b4
carlosbarreraacuna <carlos.barreraacuna@gmail.com>
2026-01-27 20:04:41 -0500

    config cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://localhost:3000'],
+    'allowed_origins' => [
+    env('FRONTEND_URL', 'http://localhost:3000'), 
+    'http://localhost:3000',
+    'https://helpdesk-frontend-xi.vercel.app'
+],
 
     'allowed_origins_patterns' => [],
 
